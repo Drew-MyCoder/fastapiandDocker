030f8b14b5eade3ddd1aae4b1ede568f90629a15
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-07-14 06:12:07 +0000

    routes for app in main file

--- api/main.py
+++ api/main.py
@@ -1,8 +1,26 @@
 from fastapi import FastAPI
+from api.utils.dbUtil import database
 
-app = FastAPI()
+app = FastAPI(
+    docs_url="/docs",
+    redoc_url="/redocs",
+    title="FastAPI (Python)",
+    description="FastAPI Framework, learning otp, email verifcation, jwt etc",
+    version="1.0",
+    openapi_url="/openapi.json",
+)
+
+
+@app.on_event("startup")
+async def startup():
+    await database.connect()
+
+
+@app.on_event("shutdown")
+async def shutdown():
+    await database.disconnect()
 
 
 @app.get('/hello')
 def hello():
-    return 'hello world and drew'
+    return 'hello world drew@@'
