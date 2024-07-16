b83ab30ca79c3495015c04ced47509726292d084
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-07-16 02:05:35 +0000

    router for login, access token

--- api/auth/router.py
+++ api/auth/router.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter, Depends, HTTPException
 from api.auth import schema
 from api.auth import crud
-from api.utils import cryptoUtil
+from api.utils import cryptoUtil, jwtUtil, constantUtil
+from fastapi.security import OAuth2PasswordRequestForm
 
 router = APIRouter(
     prefix="/api/v1"
 )
+
 
 @router.post("/auth/register", response_model=schema.UserList)
 async def register(user: schema.UserCreate):
@@ -19,3 +21,39 @@ async def register(user: schema.UserCreate):
     await crud.save_user(user)
 
     return {**user.dict()}
+
+
+@router.post("/auth/login")
+async def login(form_data: OAuth2PasswordRequestForm = Depends()):
+    #  check if user is registered
+    result = await crud.find_exist_user(form_data.username)
+    if not result:
+        raise HTTPException(
+            status_code=404, 
+            detail="User not registered.")
+
+    # verify password
+    user = schema.UserCreate(**result)
+    verified_password = cryptoUtil.verify_password(
+        form_data.password, user.password)
+    if not verified_password:
+        raise HTTPException(
+            status_code=404, 
+            detail="Incorrect username or password")
+
+    # create token
+    access_token_expires = jwtUtil.timedelta(
+        minutes=constantUtil.ACCESS_TOKEN_EXPIRE_MINUTE)
+    access_token = await jwtUtil.create_access_token(
+        data={"sub": form_data.username},
+        expires_delta=access_token_expires
+    )
+
+    return {
+        "access_token": access_token,
+        "token_type": "bearer",
+        "user_info": {
+            "email": user.email,
+            "fullname": user.fullname
+        }
+    }
