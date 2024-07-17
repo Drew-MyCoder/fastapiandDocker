53cbd41488306055e15934b4b67a94960930d1b7
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-07-17 03:15:12 +0000

    crud and email utils for forgot password

--- api/auth/crud.py
+++ api/auth/crud.py
@@ -10,3 +10,8 @@ def find_exist_user(email: str):
 def save_user(user: schema.UserCreate):
     query = "INSERT INTO py_users VALUES (nextval('user_id_seq'), :email, :password, :fullname, now() at time zone 'UTC', '1')"
     return database.execute(query, values={"email":user.email, "password":user.password, "fullname":user.fullname})
+
+
+def create_reset_code(email: str, reset_code: str):
+    query = "INSERT INTO py_codes VALUES (nextval('code_id_seq'), :email, :reset_code, '1', now() at time zone 'UTC')"
+    return database.execute(query, values={"email": email, "reset_code": reset_code})
---
+++ api/utils/emailUtil.py
@@ -0,0 +1,28 @@
+from typing import List
+from fastapi_mail import FastMail, MessageSchema, ConnectionConfig
+from starlette.config import Config
+
+# load .env 
+config = Config(".env")
+
+conf = ConnectionConfig(
+    MAIL_USERNAME=config("MAIL_USERNAME"),
+    MAIL_PASSWORD=config("MAIL_PASSWORD"),
+    MAIL_FROM=Config("MAIL_FROM"),
+    MAIL_PORT=config("MAIL_PORT"),
+    MAIL_SERVER=config("MAIL_SERVER"),
+    MAIL_TLS=config("MAIL_TLS"),
+    MAIL_SSL=config("MAIL_SSL"),
+    USE_CREDENTIALS=config("USE_CREDENTIALS")
+)
+
+
+async def send_mail(subject: str, recipient: List, message: str):
+    message = MessageSchema(
+        subject=subject,
+        recipients=recipient,
+        body=message,
+        subtype="html"
+    )
+    fm = FastMail(conf)
+    await fm.send_message(message)
