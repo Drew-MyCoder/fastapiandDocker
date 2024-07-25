d026d33565dc76395e60fdd1c259cc904ac86d46
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-07-25 03:15:31 +0000

    schema for user CRUD

--- api/auth/schema.py
+++ api/auth/schema.py
@@ -1,9 +1,14 @@
 from pydantic import BaseModel, Field
+from typing import Optional
+from datetime import datetime
 
 
 class UserList(BaseModel):
+    id: int = None
     email: str
     fullname: str
+    created_on: Optional[datetime] = None
+    status: str = None
 
 
 class UserCreate(UserList):
