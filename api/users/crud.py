5abc0a923f670fe101fab47470f7795788d082d3
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-07-25 03:58:37 +0000

    crud for change user password

--- api/users/crud.py
+++ api/users/crud.py
@@ -13,3 +13,12 @@ def update_user(
 def deactivate_user(current_user: auth_schema.UserList):
     query = "UPDATE py_users SET status='9' WHERE status='1' AND email=:email"
     return database.execute(query, values={"email": current_user.email})
+
+
+def change_password(
+    change_password_object: user_schema.ChangePassword,
+    current_user: auth_schema.UserList
+):
+    query = "UPDATE py_users SET password=:password WHERE status='1' AND email=:email"
+    return database.execute(query, values={"password": change_password_object.new_password, "email": current_user.email})
+
