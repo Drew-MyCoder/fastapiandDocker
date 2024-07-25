1ed1d464d3a68313ce36556736d15d594b4ea19f
Drew-MyCoder <andrewsdwomoh9@gmail.com>
2024-07-25 03:58:53 +0000

    router for change user password

--- api/users/router.py
+++ api/users/router.py
@@ -1,7 +1,7 @@
-from fastapi import APIRouter, Depends, status
-from api.auth import schema as auth_schema
-from api.users import schema as user_schema, crud
-from api.utils import jwtUtil
+from fastapi import APIRouter, Depends, status, HTTPException
+from api.auth import schema as auth_schema, crud as auth_crud
+from api.users import schema as user_schema, crud as user_crud
+from api.utils import jwtUtil, cryptoUtil
 
 router = APIRouter(
     prefix="/api/v1"
@@ -22,7 +22,7 @@ async def update_profile(
     jwtUtil.get_current_user
 )):
     # update user info
-    await crud.update_user(request, current_user)
+    await user_crud.update_user(request, current_user)
 
     return {
         "status_code": status.HTTP_200_OK,
@@ -35,8 +35,42 @@ async def deactivate_account(
     current_user: auth_schema.UserList = Depends(jwtUtil.get_current_active_user)
 ):
     # delete user
-    await crud.deactivate_user(current_user)
+    await user_crud.deactivate_user(current_user)
     return {
         "status_code": status.HTTP_200_OK,
         "detail": "User account has been deactivated successfully"
+    }
+
+
+@router.patch("/user/change-password")
+async def change_password(
+    change_password_object: user_schema.ChangePassword,
+    current_user: auth_schema.UserList = Depends(
+        jwtUtil.get_current_user
+)
+):
+     # check if user exists
+    result = await auth_crud.find_exist_user(current_user.email)
+    if not result:
+        # raise HTTPException(status_code=404, detail="User already registered.")
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+
+    # verify current password
+    user = auth_schema.UserCreate(**result)
+    valid = cryptoUtil.verify_password(change_password_object.current_password, user.password)
+    if not valid:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Current password is not correct")
+
+    # check new password matches with confirm password
+    if change_password_object.new_password != change_password_object.confirm_password:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="New passwords must match")
+
+    # change password
+    change_password_object.new_password = cryptoUtil.hash_password(
+        change_password_object.new_password
+    )
+    await user_crud.change_password(change_password_object, current_user)
+    return {
+        "status_code": status.HTTP_200_OK,
+        "detail": "Password has been changed successfully"
     }
